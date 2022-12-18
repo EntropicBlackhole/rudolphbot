ffd110377e23a83105913a8474648bcdb8b4056d
EntropicBlackhole <82614490+EntropicBlackhole@users.noreply.github.com>
2022-12-18 12:49:54 -0500

    Turns out the random-memes module isn't compatible with 32 bit on my pc so i downloaded like 15 memes, only to realize the random meme api was there this entire time. I hate myself, it's finished now

--- database/commands/claim.js
+++ database/commands/claim.js
@@ -1,5 +1,4 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
-const memes = require('random-memes')
 const functions = require('../bot/functions')
 const fs = require('fs');
 module.exports = {
@@ -17,14 +16,22 @@ module.exports = {
 			fs.writeFileSync('./database/misc/gifts.json', JSON.stringify(gifts, null, 2));
 		}
 		if (gifts[interaction.user.id] == 0) return interaction.editReply('You don\'t have any gifts to claim, win a snowfight or win a cookie catch to get gifts to claim!')
-		let meme = await memes.random()
+		let response = await fetch("https://random-memes-api.vercel.app/getmeme");
+		let memeObject = await response.json();
+		let meme = {
+			image: memeObject.image,
+			category: memeObject.category,
+			caption: memeObject.caption,
+			id: memeObject.id
+		}
 		const memeEmbed = new EmbedBuilder()
 			.setTitle(meme.caption)
-			.setDescription(meme.category)
 			.setImage(meme.image)
 			.setColor(functions.randomColor())
 			.setTimestamp()
 			.setFooter({ text: "Please report any bugs! Thanks! ^^", iconURL: client.user.avatarURL() });
+
+		if (meme.category != '') memeEmbed.setDescription(meme.category)
 		gifts[interaction.user.id] -= 1;
 		fs.writeFileSync('./database/misc/gifts.json', JSON.stringify(gifts, null, 2));
 		return interaction.editReply({ content: `You have: \`${gifts[interaction.user.id]}\` gift${gifts[interaction.user.id] != 1 ? 's': ''} remaining!`, embeds: [memeEmbed] })
